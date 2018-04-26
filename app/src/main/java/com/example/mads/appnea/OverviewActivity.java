7d4365d11391781db81ffe2417d1161534e58b7f
Mads Nielsen <201505813@post.au.dk>
2018-04-26 13:08:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mads/appnea/OverviewActivity.java
@@ -0,0 +1,26 @@
+package com.example.mads.appnea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class OverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_overview);
+
+        Button test = (Button) findViewById(R.id.button);
+
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(OverviewActivity.this, "Test",Toast.LENGTH_LONG).show();
+            }
+        });
+        //Dette er en test af githubd
+    }
+}

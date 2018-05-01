138dde7531f5cff14114b619a3404f87dedc482f
Oliver Ahrenfeldt <201505458@post.au.dk>
2018-05-01 12:42:36 +0200

    Merge remote-tracking branch 'origin/Main' into Main
    # Conflicts:
    #	app/src/main/java/com/example/mads/appnea/LoginActivity.java

---
+++ app/src/main/java/com/example/mads/appnea/DataModel.java
@@ -0,0 +1,19 @@
+package com.example.mads.appnea;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.TreeMap;
+
+public class DataModel {
+
+    private Date timeStamp;
+    private boolean sync;
+    private ArrayList<Integer> data;
+
+    public DataModel()
+    {
+        data = null;
+        sync = false;
+        timeStamp = null;
+    }
+}
